c00758e785c97dd7a48bd4fd4eaa370dc8678f31
Tom VanAntwerp <tom@tomvanantwerp.com>
2017-11-13 10:50:09 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Options from './components/Options';
+import Timer from './components/Timer';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timerLength: 15,
+      timerStarted: false
+    }
+
+    this.updateTimerLength = this.updateTimerLength.bind(this);
+    this.startTimer = this. startTimer.bind(this);
+  }
+
+  updateTimerLength(time) {
+    this.setState({timerLength: time});
+  }
+
+  startTimer() {
+    this.setState({timerStarted: true});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.timerStarted
+          ? <Timer time={this.state.timerLength} />
+          : <Options
+              updateTimerLength={this.updateTimerLength}
+              startTimer={this.startTimer}  
+            />
+        }
+        
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Options.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Options extends Component {
+  render() {
+    return <div className="options" />;
+  }
+}
+
+export default Options;

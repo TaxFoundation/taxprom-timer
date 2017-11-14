587ea7909702b30a43647fc353aca947fcfc8c36
Tom VanAntwerp <tom@tomvanantwerp.com>
2017-11-14 12:40:42 -0500

    Complete basic styles until Dan gets me new styles

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,12 +4,20 @@ class Options extends Component {
   render() {
     return (
       <div className="options">
+        <label className="options__label" htmlFor="minutes">Minutes for Countdown</label>
         <input
+          className="options__minutes"
+          id="minutes"
           type="number"
           value={this.props.timerLength}
           onChange={event => this.props.updateTimerLength(event.target.value)}
         />
-        <button onClick={this.props.startTimer}>Start Timer</button>
+        <button
+          className="options__start"
+          onClick={this.props.startTimer}
+        >
+          Start Timer
+        </button>
       </div>
     );
   }

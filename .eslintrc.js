dead3bf5b358c98de7fd29cafeeaf441070d69f5
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-11-12 13:20:31 -0500

    Update eslint config and remove unused files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'prettier/react'
+  ],
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
@@ -11,12 +15,18 @@ module.exports = {
     },
     sourceType: 'module'
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'react/jsx-uses-vars': 2
+    'react/jsx-uses-vars': 2,
+    'react/jsx-uses-react': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 120
+      }
+    ]
   }
 };
